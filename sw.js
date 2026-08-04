a3df59d505a24ab50f37f5edb17eb116cea9909f
barakaflalo <barakaflalo4780@gmail.com>
2026-08-04 22:55:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,5 @@
-const CACHE_NAME = 'sunoprep-v9.1.1';
-const ASSETS = [
-  './',
-  './index.html?v=9.1.1',
-  './app.jsx?v=9.1.1',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone@7/babel.min.js'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('api.anthropic.com')) return;
-  e.respondWith(
-    fetch(e.request).then(res => {
-      if (res.status === 200) {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-      }
-      return res;
-    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html'))))
-  );
-});
+// SunoPrep 9.1.2: retire stale caches.
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.registration.unregister())
+));
